4b1fd06dafd9a9bb5c5f4876d35a6c7f4a83f11f
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-03-08 20:59:47 +0530

    update input processing

--- Test-ChargingCurrent.cpp
+++ Test-ChargingCurrent.cpp
@@ -1,1 +1,9 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include "test/catch.hpp"
+
+/*Test case for current sampling input*/
+TEST_CASE("Current sampling input test") {
+  int current_sample_array[5]={2,2,3,4,5};
+  REQUIRE(sampling_Input(&current_sample_array,5) > 0);
+}
