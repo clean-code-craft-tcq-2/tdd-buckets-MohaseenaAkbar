44a7acf8a5d85d36d910ad6feef72a0406c7158d
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-03-09 12:05:36 +0530

    Update Current-sample.c

--- Current-sample.c
+++ Current-sample.c
@@ -3,12 +3,13 @@
 
 Charge_Range sampling_Input(int* current_sample,int NoOfSample)
 {
+  Charge_Range_Ver Charge_Range;
   int Lower_cnt;
   /*processing input values*/
   for(i=0;(current_sample[i]<NoOfSample);i++)
   {
     if((current_sample[i]>=3)&&(current_sample[i]<=5))
-     Lower_cnt++;
+     Charge_Range.Lower_cnt++;
   }
-  return Lower_cnt;
+  return Charge_Range;
 }
