cb97195202d078647e3741a83fe191c9d4e8d8f6
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-03-09 11:47:52 +0530

    Update Test-ChargingCurrent.cpp

--- Test-ChargingCurrent.cpp
+++ Test-ChargingCurrent.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "test/catch.hpp"
+//#include "test/catch.hpp"
 
 /*Test case for current sampling input*/
 TEST_CASE("Current sampling input test for range 3-5") {
