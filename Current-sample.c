7bd82c636a1443de85e7b4c813d6a6047c34386b
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-04-22 15:37:25 +0530

    Update Current-sample.c

--- Current-sample.c
+++ Current-sample.c
@@ -1,14 +1,7 @@
 /*Code to capture chaging ranges*/
 #include "Current-sample.h"
 
-Charge_Range sampling_Input(int* current_sample,int NoOfSample)
+void Sort_Current_Samples(int* current_sample,int NoOfSample)
 {
-  Charge_Range Charge_Range_Loc;
-  /*processing input values*/
-  for(i=0;(current_sample[i]<NoOfSample);i++)
-  {
-    if((current_sample[i]>=3)&&(current_sample[i]<=5))
-     Charge_Range_Loc.Lower_cnt++;
-  }
-  return Charge_Range_Loc;
+ /*
 }
