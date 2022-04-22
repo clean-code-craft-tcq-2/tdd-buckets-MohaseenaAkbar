8504907972551f6ad6c4ac7d370788c08cf0cba5
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-04-22 15:37:31 +0530

    Update Test-ChargingCurrent.cpp
    Sort the input current samples

--- Test-ChargingCurrent.cpp
+++ Test-ChargingCurrent.cpp
@@ -9,13 +9,13 @@ TEST_CASE("sorting the Input array")
   int current_sample_arr[]={2,8,10,15,3,4,5};
   int expected_sorted_arr[]={2,3,4,5,8,10,15};
   boolean isArrayEqual=true;
-  sampling_Input_Sort(&current_sample_array,5);
-	for (int i = 0; i < arr2.Length; i++) 
+  Sort_Current_Samples(&current_sample_array,5);
+  for (int i = 0; i < arr2.Length; i++) 
   {
-			if (expected_sorted_arr[i] != current_sample_arr[i]) 
+      if (expected_sorted_arr[i] != current_sample_arr[i]) 
       {
           isArrayEqual = false;
-			}
+      }
   }
   REQUIRE(isArrayEqual == true);
 }
