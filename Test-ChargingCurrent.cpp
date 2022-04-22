0e1e420df8a45a4333bec2d4f1336e2b5802c759
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-04-22 15:47:38 +0530

    Update Test-ChargingCurrent.cpp

--- Test-ChargingCurrent.cpp
+++ Test-ChargingCurrent.cpp
@@ -10,7 +10,8 @@ TEST_CASE("sorting the Input array")
   int expected_sorted_arr[]={2,3,4,5,8,10,15};
   bool isArrayEqual=true;
   int Current_Sample,Sample_Size=5;
-  Sort_Current_Samples(&current_sample_array,Sample_Size);
+  /*Sort and verify the current samples*/
+  Sort_Current_Samples(&current_sample_arr,Sample_Size);
   for (int Current_Sample = 0; Current_Sample < Sample_Size; Current_Sample++) 
   {
       if (expected_sorted_arr[Current_Sample] != current_sample_arr[Current_Sample]) 
