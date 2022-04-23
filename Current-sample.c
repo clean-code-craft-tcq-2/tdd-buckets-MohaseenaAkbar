9e143f052d955dd7d95f557d0980c1db3faa2e52
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-04-23 17:48:40 +0530

    Update Current-sample.c

--- Current-sample.c
+++ Current-sample.c
@@ -4,9 +4,9 @@
 void Sort_Current_Samples(int* current_sample_arr,int NoOfSample)
 {
   int current_sample1,current_sample2,swap_var;
-  for (current_sample1 = 0 ; current_sample1 < size - 1; current_sample1++)
+  for (current_sample1 = 0 ; current_sample1 < NoOfSample - 1; current_sample1++)
   {
-      for (current_sample2 = 0 ; current_sample2 < size - current_sample1 - 1; current_sample2++)
+      for (current_sample2 = 0 ; current_sample2 < NoOfSample - current_sample1 - 1; current_sample2++)
       {
          if (current_sample_arr[current_sample2] > current_sample_arr[current_sample2+1]) /* For decreasing order use < */
          {
