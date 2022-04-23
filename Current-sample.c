417f5d968588f48cb396768d7b0b3bdf5629217d
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-04-23 15:53:33 +0530

    Update Current-sample.c

--- Current-sample.c
+++ Current-sample.c
@@ -1,7 +1,19 @@
 /*Code to capture chaging ranges*/
 #include "Current-sample.h"
 
-void Sort_Current_Samples(int* current_sample,int NoOfSample)
+void Sort_Current_Samples(int* current_sample_arr,int NoOfSample)
 {
- /* Code to sort the current samples*/
+  int current_sample1,current_sample2,swap_var;
+  for (current_sample1 = 0 ; current_sample1 < size - 1; current_sample1++)
+  {
+      for (current_sample2 = 0 ; current_sample2 < size - current_sample1 - 1; current_sample2++)
+      {
+         if (current_sample[counter1] > current_sample[counter1+1]) /* For decreasing order use < */
+         {
+             swap_var        = current_sample_arr[current_sample2];
+             current_sample_arr[current_sample2]   = current_sample_arr[current_sample2+1];
+             current_sample_arr[current_sample2+1] = swap_var;
+         }
+      }
+  }
 }
